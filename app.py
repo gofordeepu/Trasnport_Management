a07e00b498361aa8af70ca0f181994b0721b15de
Deepu <gofordeepu@gmail.com>
2022-11-24 11:50:52 +0530

    created new templates for corresponding pages and added some routes in app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
-
+# homepage
 @app.route('/')
 def home():
     return render_template("home.html")
+    
+# see transactions
+@app.route('/transactions')
+def see_transactions():
+    return render_template("see_transactions.html")
+
+# add transactions
+@app.route('/addtransactions',methods=['GET','POST'])
+def add_transactions():
+    if request.method=='POST':
+        print(request.form['company'])
+        return render_template('add_transactions.html')
+    else:
+        return render_template("add_transactions.html")
+
+# see vehicals
+@app.route('/vehicals')
+def see_vehicals():
+    return render_template("see_vehicals.html")
+
  
 # main driver function
 if __name__ == '__main__':
